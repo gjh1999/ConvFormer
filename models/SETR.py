2e6a473ae00769a32def3c34d68a0fa00afd9712
gjh1999 <55343492+gjh1999@users.noreply.github.com>
2023-12-05 21:31:52 +0800

    Update SETR.py

--- models/SETR.py
+++ models/SETR.py
@@ -13,13 +13,19 @@ from models.components.anti_over_smoothing import Transformer_Reattention, Trans
 def pair(t):
     return t if isinstance(t, tuple) else (t, t)
 
-def relative_pos_dis(height=32, weight=32, sita=0.9):
+def relative_pos_dis(height=32, weight=32, sita=0.9):#函数 relative_pos_dis 接受三个参数：
+    #height：网格的高度（默认为32）。
+#weight：网格的宽度（默认为32）。
+#sita：计算中使用的一个参数（默认为0.9）。
     coords_h = torch.arange(height)
     coords_w = torch.arange(weight)
     coords = torch.stack(torch.meshgrid([coords_h, coords_w]))  # 2, Wh, Ww # 0 is 32 * 32 for h, 1 is 32 * 32 for w
-    coords_flatten = torch.flatten(coords, 1)  # 2, Wh*Ww
-    relative_coords = coords_flatten[:, :, None] - coords_flatten[:, None, :]  # 2, Wh*Ww, Wh*Ww
-    relative_coords = relative_coords.permute(1, 2, 0).contiguous()  # Wh*Ww, Wh*Ww, 2
+    '''
+    这部分使用 torch.meshgrid 创建了一个二维坐标网格。得到的 coords 张量的形状为 (2, height, weight)，其中第一个维度表示两个坐标轴，接下来两个维度表示网格的高度和宽度。
+    '''
+    coords_flatten = torch.flatten(coords, 1)  # 2, Wh*Ww#将坐标展平成形状为 (2, Wh*Ww) 的二维张量。此行计算了展平坐标张量中所有点的相对坐标。结果是一个形状为 (2, WhWw, WhWw) 的张量，其中第一个维度表示坐标轴，接下来两个维度表示成对的相对坐标。
+    relative_coords = coords_flatten[:, :, None] - coords_flatten[:, None, :]  # 2, Wh*Ww, 此行计算了展平坐标张量中所有点的相对坐标。结果是一个形状为 (2, WhWw, WhWw) 的张量，其中第一个维度表示坐标轴，接下来两个维度表示成对的相对坐标。
+    relative_coords = relative_coords.permute(1, 2, 0).contiguous()  # Wh*Ww, Wh*Ww, 2Wh*Ww将张量的维度置换为 (WhWw, WhWw, 2)，以便更容易处理后续步骤。
     dis = (relative_coords[:, :, 0].float()/height) ** 2 + (relative_coords[:, :, 1].float()/weight) ** 2
     #dis = torch.exp(-dis*(1/(2*sita**2)))
     return  dis
@@ -31,7 +37,7 @@ class PreNorm(nn.Module):
         self.fn = fn
     def forward(self, x, **kwargs):
         return self.fn(self.norm(x), **kwargs)
-
+#归一化层
 class CNNPreNorm(nn.Module):
     def __init__(self, dim, fn):
         super().__init__()
@@ -39,7 +45,7 @@ class CNNPreNorm(nn.Module):
         self.fn = fn
     def forward(self, x, **kwargs):
         return self.fn(self.norm(x), **kwargs)
-
+#cnn归一化层
 
 class FeedForward(nn.Module):
     def __init__(self, dim, hidden_dim, dropout=0.):
@@ -53,7 +59,7 @@ class FeedForward(nn.Module):
         )
     def forward(self, x):
         return self.net(x)
-
+#前向传播层
 
 class CNNFeedForward(nn.Module):
     def __init__(self, dim, hidden_dim, dropout=0.):
@@ -68,7 +74,7 @@ class CNNFeedForward(nn.Module):
         )
     def forward(self, x):
         return self.net(x)
-
+#cnn    前向传播层
 
 class Attention(nn.Module):
     def __init__(self, dim, heads=8, dim_head=64, dropout=0.):
@@ -86,7 +92,23 @@ class Attention(nn.Module):
             nn.Linear(inner_dim, dim),
             nn.Dropout(dropout)
         ) if project_out else nn.Identity()
+'''
+初始化：
 
+dim: 输入张量的最后一个维度大小。
+heads: 多头自注意力机制的头数，默认为8。
+dim_head: 每个头的维度，默认为64。
+dropout: 用于输出的dropout概率，默认为0。
+属性:
+
+inner_dim: 头数和头维度的乘积，表示内部的维度。
+project_out: 一个布尔值，表示是否需要将输出投影到原始维度。
+self.heads: 多头注意力机制中的头数。
+self.scale: 缩放因子，用于调整点积的缩放。
+self.attend: 用于计算注意力分布的Softmax层。
+self.to_qkv: 将输入投影到查询（q）、键（k）、值（v）的线性层。
+self.to_out: 输出层，将注意力机制的结果投影回原始维度。
+'''
     def forward(self, x, mode="train"):
         qkv = self.to_qkv(x).chunk(3, dim=-1)
         q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=self.heads), qkv)
@@ -100,9 +122,19 @@ class Attention(nn.Module):
             return self.to_out(out)
         else:
             return self.to_out(out), attn
+            '''
+            前向传播：
+将输入 x 通过线性层 self.to_qkv 投影为查询（q）、键（k）、值（v）。
+使用 rearrange 函数对查询、键、值进行形状变换，以适应多头自注意力机制。
+计算点积得到注意力分数，并通过缩放因子 self.scale 进行缩放。
+对得到的点积进行缩放，得到注意力分布。
+使用Softmax计算注意力分布。
+将注意力分布应用于值，得到加权和。
+最后，通过线性层 self.to_out 进行投影，并根据模式选择返回结果。
+'''
+#注意力层 
 
-
-class CNNAttention(nn.Module):
+class CNNAttention(nn.Module):#卷积注意力机制 加了一个根据距离的权重
     def __init__(self, dim, heads=8, dim_head=64, dropout=0., num_patches=1024):
         super().__init__()
         inner_dim = dim_head * heads
@@ -123,7 +155,25 @@ class CNNAttention(nn.Module):
             nn.BatchNorm2d(dim), # inner_dim
             nn.ReLU(inplace=True),
         ) if project_out else nn.Identity()
+'''
+dim: 输入张量的通道数。
+heads: 多头自注意力机制的头数，默认为8。
+dim_head: 每个头的维度，默认为64。
+dropout: 用于输出的dropout概率，默认为0。
+num_patches: 图像被划分的块数，默认为1024。
+属性:
 
+inner_dim: 头数和头维度的乘积，表示内部的维度。
+project_out: 一个布尔值，表示是否需要将输出投影到原始维度。
+self.heads: 多头注意力机制中的头数。
+self.scale: 缩放因子，用于调整点积的缩放。
+self.num_patches: 图像被划分的块数。
+self.to_qkv: 将输入图像的通道映射到查询（q）、键（k）、值（v）的卷积层。
+self.dis: 通过调用 relative_pos_dis 函数计算的相对位置矩阵。
+self.headsita: 注意力矩阵的权重，通过Sigmoid激活。
+self.sig: Sigmoid激活函数。
+self.to_out: 输出层，将注意力机制的结果投影回原始维度。
+'''
     def forward(self, x, mode="train", smooth=1e-4):
         qkv = self.to_qkv(x).chunk(3, dim=1)
         q, k, v = map(lambda t: rearrange(t, 'b (g d) h w -> b g (h w) d', g=self.heads), qkv)
@@ -146,7 +196,16 @@ class CNNAttention(nn.Module):
             return self.to_out(out)
         else:
             return self.to_out(out), attn
-
+'''
+前向传播：
+将输入 x 通过卷积层 self.to_qkv 投影为查询（q）、键（k）、值（v）。
+使用 rearrange 函数对查询、键、值进行形状变换，以适应多头自注意力机制。
+计算点积得到注意力分数。
+对查询和键的范数进行归一化，防止数值不稳定。
+计算关注矩阵，并通过相对位置矩阵进行加权。
+将注意力矩阵应用于值，得到加权和。
+最后，通过卷积层 self.to_out 进行投影，并根据模式选择返回结果。
+'''
 
 class Transformer(nn.Module):
     def __init__(self, dim, depth, heads, dim_head, mlp_dim=1024, dropout=0., num_patches=128):
@@ -162,9 +221,22 @@ class Transformer(nn.Module):
             x = attn(x) + x
             x = ff(x) + x
         return x
+'''
+初始化：
 
+dim: 输入张量的最后一个维度大小。
+depth: Transformer模型的层数。
+heads: 自注意力机制中的头数。
+dim_head: 每个头的维度。
+mlp_dim: MLP（多层感知机）中的隐藏层维度，默认为1024。
+dropout: 用于注意力机制和MLP的dropout概率，默认为0。
+num_patches: 图像被划分的块数，默认为128。
+属性:
 
-class Transformer_record(nn.Module):
+self.layers: 一个包含Transformer每一层的 nn.ModuleList。
+'''
+
+class Transformer_record(nn.Module):#可视化模块 可以记录 
     def __init__(self, dim, depth, heads, dim_head, mlp_dim=1024, dropout=0., num_patches=128):
         super().__init__()
         self.layers = nn.ModuleList([])
@@ -173,11 +245,17 @@ class Transformer_record(nn.Module):
                 PreNorm(dim, Attention(dim, heads=heads, dim_head=dim_head, dropout=dropout)),
                 PreNorm(dim, FeedForward(dim, mlp_dim, dropout=dropout))
             ]))
+            #与前述 Transformer 类似，包含Transformer每一层的 nn.ModuleList。
     def forward(self, x):
         for attn, ff in self.layers:
             x = attn(x) + x
             x = ff(x) + x
         return x
+        '''
+        前向传播：
+与前述 Transformer 类似，通过每一层的注意力机制（Attention）和MLP（FeedForward）进行多层迭代。
+在每一层结束时，将输入与层的输出相加，以便进行残差连接。
+'''
     def infere(self, x):
         ftokens, attmaps = [], []
         for attn, ff in self.layers:
@@ -192,7 +270,11 @@ class Transformer_record(nn.Module):
             ftokens.append(x)
             attmaps.append(amap)
         return x, ftokens, attmaps
-
+'''
+推理：
+与前述的前向传播类似，不同之处在于它在每一层注意力机制的输出中记录了注意力矩阵和中间特征张量。
+使用 mode="record" 参数调用每一层的注意力机制。
+'''
 
 # ================================ components of improved models ================================
 
